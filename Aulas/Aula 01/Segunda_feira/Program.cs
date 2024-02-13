61858377b0629f277e81e83cf8b3d3951aec99bb
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2024-02-13 11:11:08 -0300

    Inclusão de anotações

---
+++ Aulas/Aula 01/Segunda_feira/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more informatio
+
+using System; //Biblioteca de entrada e saída
+class Program{
+    static void Main(string[] args){
+        System.Console.Write("Informe o nome: ");
+        String nome = System.Console.ReadLine();
+        System.Console.Write("Amendoim " + nome);
+    } 
+}
