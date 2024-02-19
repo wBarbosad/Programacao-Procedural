e49053d11de15605628089cc5e01cfbeb93e33ea
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2024-02-19 20:03:39 -0300

    Lista 1 - Solução de exercício como base para monitoria

---
+++ Aulas/Aula03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args){
+        int idade;
+        bool validacaoIdade;
+
+        idade = int.Parse(Console.ReadLine());
+        validacaoIdade = (idade>=18);
+        Console.WriteLine("Essa pessoa é maior de idade? " + validacaoIdade);
+    }
+}
---
+++ Listas/Lista1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class Program
+{    static void Main(string[] args)
+    {
+        double[] notas = new double[4];
+        double soma = 0, media;
+        int i;
+
+        for(i=0; i<4; i++)
+        {
+            Console.WriteLine("Informe a nota" + (i+1));
+            notas[i] = double.Parse(Console.ReadLine());
+            soma = soma+notas[i];
+        }
+        media = soma/i;
+        Console.WriteLine("A média das " + i + "notas = " + media);
+    }
+}
