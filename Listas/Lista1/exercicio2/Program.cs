abe055e88e4d4490895623679074064f5a9393de
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2024-02-19 21:15:37 -0300

    Problema no exercício 3

---
+++ Listas/Lista1/exercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program(){
+    static void Main(){
+    
+        // “A área do triângulo é maior que 20? False/True”
+        double altura, baseTriangulo, area;
+        bool validacaoArea;
+
+        altura = double.Parse(Console.ReadLine());
+        baseTriangulo = double.Parse(Console.ReadLine());
+        area = (altura*baseTriangulo)/2;
+
+        Console.WriteLine("“A área do triângulo é maior que 20? " + (validacaoArea = (area>=20)));
+        
+    }
+}
---
+++ Listas/Lista1/exercicio3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program(){
+    static void Main(string[] args){
+        double moedas_centavos;
+        double conversorInteiro, conversor;
+
+        moedas_centavos = int.Parse(Console.ReadLine());
+        conversor = moedas_centavos/100;
+        conversorInteiro = Math.Round(conversor);
+        Console.WriteLine("Reais: " + conversorInteiro + " Fracionado " + );
+
+    }
+}
