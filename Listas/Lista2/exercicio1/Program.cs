2b9b38f3bc541355989fcc2c45fce70ed72fca69
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2024-02-29 22:52:04 -0300

    Exercicio 1 - Lista 2

---
+++ Listas/Lista2/exercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Program{
+    static void Main(string[] args){
+        string nome = Console.ReadLine();
+        string senha = Console.ReadLine();
+
+        if (senha=="123senha" && nome=="admin"){
+            System.Console.WriteLine("Login realizado com sucesso!");
+        }
+        else{
+            System.Console.WriteLine("Credenciais inválidas!");
+        }
+    }
+}
