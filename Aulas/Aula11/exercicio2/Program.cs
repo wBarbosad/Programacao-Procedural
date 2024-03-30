670e6b74341c352311120b82a098bebd433c6304
Wesley Barbosa da Silva <124535880+wBarbosad@users.noreply.github.com>
2024-03-30 09:25:30 -0300

    Aulas

---
+++ Aulas/Aula11/exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args)
+    {
+        string[] nome = new string[3];
+        
+
+        for(byte i = 0; i<nome.Length; i++){
+            nome[i] = Console.ReadLine();
+            if(nome[i]=="Jose"){    
+                
+            }
+            else{
+                System.Console.WriteLine("Nome não encontrado.");
+            }
+        }
+    }
+    
+}
---
+++ Listas/Lista6/exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program{
+        const int TAMANHO_NOME = 10;
+    static void Main(string[] arg){
+        string[] nome = new string[10];
+
+
+    } 
+
+
+}
